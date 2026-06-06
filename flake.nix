f8820d342a58209613799d5833014884c33d992d
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-06-06 16:10:53 +0900

    add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "flake for obsidian activity log";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import an internal flake module: ./other.nix
+        # To import an external flake module:
+        #   1. Add foo to inputs
+        #   2. Add foo as a parameter to the outputs function
+        #   3. Add here: foo.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+          ];
+          
+          shellHook = ''
+            echo "completed!"
+            echo "Node.js version: $(node -v)"
+          '';
+        };
+      };
+    };
+}
